dd894d8680c3fa0f7a7fa444be16d14d9b7110d1
user <kriviz.alexandra@gmail.com>
2025-06-28 00:18:33 +0200

    GSP-26 i've added ProductRepositiry and ProductService.

---
+++ src/main/java/de/telran/gardenStore/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package de.telran.gardenStore.repository;
+
+import de.telran.gardenStore.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}

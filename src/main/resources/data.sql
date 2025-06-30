0ad018141dbc81609a07d5e2412609838db6d2c7
NatKond <natzubova@gmail.com>
2025-06-30 10:30:45 +0200

    GSP-36 Changes to database initialisation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 TRUNCATE TABLE app_users RESTART IDENTITY;
 
--- INSERT INTO app_users (name, email, phone_number, password_hash, role)
--- VALUES
---     ('Alice Johnson', 'alice.johnson@example.com', '+1234567890', 'hashed_password_1', 'ROLE_USER'),
---     ('Bob Smith', 'bob.smith@example.com', '+1987654321', 'hashed_password_2', 'ROLE_USER'),
---     ('Carol Lee', 'carol.lee@example.com', '+1122334455', 'hashed_password_3', 'ROLE_USER'),
---     ('David Brown', 'david.brown@example.com', '+1222333444', 'hashed_password_4', 'ROLE_USER');
+INSERT INTO app_users (name, email, phone_number, password_hash, role)
+VALUES
+    ('Alice Johnson', 'alice.johnson@example.com', '+1234567890', '12345', 'ROLE_USER'),
+    ('Bob Smith', 'bob.smith@example.com', '+1987654321', '12345', 'ROLE_USER'),
+    ('Carol Lee', 'carol.lee@example.com', '+1122334455', '12345', 'ROLE_USER'),
+    ('David Brown', 'david.brown@example.com', '+1222333444', '12345', 'ROLE_USER');
 
 TRUNCATE TABLE products RESTART IDENTITY;
 INSERT INTO products (name, discount_price, price,category_id, description, image_url)

9d324e2c6fe42628830a1d9233291e4b2d4fdebd
dima202513 <dmitrijra@gmail.com>
2025-07-10 21:16:57 +0200

    Create Order, OrderItem entities, Repository and Service layers

---
+++ src/main/java/de/telran/gardenStore/controller/OrderController.java
@@ -0,0 +1,38 @@
+package de.telran.gardenStore.controller;
+
+import de.telran.gardenStore.entity.Order;
+import de.telran.gardenStore.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
+}

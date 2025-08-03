47bc1d18c4852e041aaeecad353950ccf0691b8a
NatKond <natzubova@gmail.com>
2025-08-03 22:22:40 +0200

    GSP-81 Fixed bug in ProductRepository

--- src/main/java/de/telran/gardenStore/repository/ProductRepository.java
+++ src/main/java/de/telran/gardenStore/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import de.telran.gardenStore.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p " +
-            "FROM OrderItem oi "+
+            "FROM OrderItem oi " +
             "JOIN oi.product p " +
             "JOIN oi.order o " +
             "WHERE o.status = :orderStatus " +
@@ -24,7 +25,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> getTopWithStatus(OrderStatus orderStatus, Integer limit);
 
     @Query("SELECT p " +
-            "FROM OrderItem oi "+
+            "FROM OrderItem oi " +
             "JOIN oi.product p " +
             "JOIN oi.order o " +
             "WHERE o.status = :orderStatus AND o.updatedAt < :days " +
@@ -33,16 +34,15 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "LIMIT :limit"
     )
     List<Product> getAwaitingPaymentForDays(LocalDateTime amountOfDays, Integer limit);
-}
 
     @Query(value = """
-        SELECT p.*
-        FROM products p
-        WHERE p.discount_price IS NOT NULL
-        AND FLOOR(((p.price - p.discount_price) * 100) / p.price) = (
-            SELECT MAX(FLOOR(((p.price - p.discount_price) * 100) / p.price)) 
+            SELECT p.*
             FROM products p
-        )
-        """, nativeQuery = true)
+            WHERE p.discount_price IS NOT NULL
+            AND FLOOR(((p.price - p.discount_price) * 100) / p.price) = (
+                SELECT MAX(FLOOR(((p.price - p.discount_price) * 100) / p.price))
+                FROM products p
+            )
+            """, nativeQuery = true)
     List<Product> findProductsWithHighestDiscount();
 }

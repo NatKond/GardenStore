5f06a4b4bf26a958f8bf3f7738cb76d2175644af
NatKond <natzubova@gmail.com>
2025-08-10 18:07:37 +0200

    GSP-86 Changes to ReportControllerImplTest and ReportServiceImplTest, fixed mistake in test database, changes to AbstractTest

--- src/test/java/de/telran/gardenStore/controller/ReportControllerImplTest.java
+++ src/test/java/de/telran/gardenStore/controller/ReportControllerImplTest.java
@@ -1,5 +1,6 @@
 package de.telran.gardenStore.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.telran.gardenStore.AbstractTest;
 import de.telran.gardenStore.dto.report.ProductReport;
 import de.telran.gardenStore.dto.report.ProfitReport;
@@ -11,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ReportControllerImpl.class)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class ReportControllerImplTest extends AbstractTest {
     @Autowired
     private MockMvc mockMvc;
@@ -33,92 +33,90 @@ class ReportControllerImplTest extends AbstractTest {
     @MockitoBean
     private JwtService jwtService;
 
-    @Test
-    @DisplayName("GET /v1/report/top-purchased-products/{limit} - Getting top N most ordered products")
-    @WithMockUser(roles = "ADMIN")
-    void getTopOrderedProducts_ShouldReturnList() throws Exception {
-        int limit = 3;
-        ProductReport report = ProductReport.builder()
-                .product(productShortResponseDto1)
-                .quantity(5L)
-                .build();
+    @Autowired
+    private ObjectMapper objectMapper;
 
-        when(reportService.getTopOrderedProducts(limit)).thenReturn(List.of(report));
+    @Test
+    @DisplayName("GET /v1/report/top-purchased-products/{limit} - Get most ordered products")
+    void getTopOrderedProducts() throws Exception {
+        int limit = 3;
+        List<ProductReport> expected = List.of(ProductReport.builder()
+                .product(productShortResponseDto3)
+                .quantity(1L)
+                .build());
+
+        when(reportService.getTopOrderedProducts(limit)).thenReturn(expected);
 
         mockMvc.perform(get("/v1/report/top-purchased-products/{limit}", limit))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].product.productId").value(report.getProduct().getProductId()))
-                .andExpect(jsonPath("$[0].quantity").value(report.getQuantity()));
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json(objectMapper.writeValueAsString(expected)));
     }
 
     @Test
-    @DisplayName("GET /v1/report/top-canceled-products/{limit} - Getting top N most canceled products")
-    @WithMockUser(roles = "ADMIN")
+    @DisplayName("GET /v1/report/top-canceled-products/{limit} - Get most canceled products")
+
     void getTopCanceledProducts_ShouldReturnList() throws Exception {
         int limit = 2;
-        ProductReport report = ProductReport.builder()
-                .product(productShortResponseDto2)
-                .quantity(3L)
-                .build();
+        List<ProductReport> expected = List.of(ProductReport.builder()
+                .product(productShortResponseDto1)
+                .quantity(1L)
+                .build());
 
-        when(reportService.getTopCanceledProducts(limit)).thenReturn(List.of(report));
+        when(reportService.getTopCanceledProducts(limit)).thenReturn(expected);
 
         mockMvc.perform(get("/v1/report/top-canceled-products/{limit}", limit))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].product.productId").value(report.getProduct().getProductId()));
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json(objectMapper.writeValueAsString(expected)));
     }
 
     @Test
-    @DisplayName("GET /v1/report/awaiting-payment-products - Getting products awaiting payment")
-    @WithMockUser(roles = "ADMIN")
+    @DisplayName("GET /v1/report/awaiting-payment-products - Get products awaiting payment")
     void getProductsAwaitingPaymentForDays_ShouldReturnList() throws Exception {
         int days = 5;
         int limit = 10;
-        ProductReport report = ProductReport.builder()
-                .product(productShortResponseDto3)
-                .quantity(2L)
-                .build();
+        List<ProductReport> expected = List.of(ProductReport.builder()
+                .product(productShortResponseDto1)
+                .quantity(1L)
+                .build());
 
-        when(reportService.getProductsAwaitingPaymentForMoreDays(days, limit)).thenReturn(List.of(report));
+        when(reportService.getProductsAwaitingPaymentForMoreDays(days, limit)).thenReturn(expected);
 
         mockMvc.perform(get("/v1/report/awaiting-payment-products")
                         .param("days", String.valueOf(days))
                         .param("limit", String.valueOf(limit)))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].quantity").value(report.getQuantity()));
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json(objectMapper.writeValueAsString(expected)));
     }
 
     @Test
-    @DisplayName("GET /v1/report/profit - Getting profit report")
-    @WithMockUser(roles = "ADMIN")
+    @DisplayName("GET /v1/report/profit - Get profit report")
     void getProfitOverPeriod_ShouldReturnList() throws Exception {
         String timeUnit = "months";
-        int timeAmount = 12;
+        int timeAmount = 6;
         String groupBy = "month";
 
-        ProfitReport report = ProfitReport.builder()
-                .period("2023-01")
-                .amountPerPeriod(BigDecimal.valueOf(1000))
-                .build();
+        List<ProfitReport> expected = List.of(
+                ProfitReport.builder()
+                        .period("2025-05")
+                        .amountPerPeriod(BigDecimal.valueOf(11.50))
+                        .build()
+        );
 
-        when(reportService.getProfitOverPeriod(timeUnit, timeAmount, groupBy)).thenReturn(List.of(report));
+        when(reportService.getProfitOverPeriod(timeUnit, timeAmount, groupBy)).thenReturn(expected);
 
         mockMvc.perform(get("/v1/report/profit")
                         .param("timeUnit", timeUnit)
                         .param("timeAmount", String.valueOf(timeAmount))
                         .param("groupBy", groupBy))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].period").value(report.getPeriod()))
-                .andExpect(jsonPath("$[0].amountPerPeriod").value(report.getAmountPerPeriod()));
-    }
-    @Test
-    @DisplayName("GET /v1/report/* - Access denied without ADMIN role")
-    void accessDeniedWithoutAdminRole() throws Exception {
-        mockMvc.perform(get("/v1/report/top-purchased-products/3"))
-                .andExpect(status().isForbidden());
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json(objectMapper.writeValueAsString(expected)));
     }
 }
--- src/test/java/de/telran/gardenStore/service/ReportServiceImplTest.java
+++ src/test/java/de/telran/gardenStore/service/ReportServiceImplTest.java
@@ -13,10 +13,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,76 +36,110 @@ class ReportServiceImplTest extends AbstractTest {
     private ReportServiceImpl reportService;
 
     @Test
-    @DisplayName("Getting top N most ordered products")
-    void getTopOrderedProducts_ShouldReturnList() {
-        int limit = 3;
-        List<Object[]> resultList = List.of(
-                new Object[]{1L, "Product 1", BigDecimal.valueOf(10), BigDecimal.valueOf(8), 1L, "Desc 1", "url1", 5L},
-                new Object[]{2L, "Product 2", BigDecimal.valueOf(20), BigDecimal.valueOf(15), 2L, "Desc 2", "url2", 3L}
-        );
+    @DisplayName("Get most ordered products")
+    void getTopOrderedProducts() {
+        Integer limit = 3;
+        List<Object[]> resultList = List.<Object[]>of(
+                new Object[]{
+                        3L,
+                        "Slug & Snail Barrier Pellets",
+                        BigDecimal.valueOf(5.75),
+                        BigDecimal.valueOf(7.50),
+                        2L,
+                        "Pet-safe barrier pellets to protect plants from slugs",
+                        "https://example.com/images/protection_slug_pellets.jpg",
+                        1L
+                });
+
+        List<ProductReport> expected = List.of(ProductReport.builder()
+                .product(productShortResponseDto3)
+                .quantity(1L)
+                .build());
+
 
         when(entityManager.createNativeQuery(anyString())).thenReturn(query);
         when(query.setParameter("status", OrderStatus.DELIVERED.name())).thenReturn(query);
         when(query.setParameter("limit", limit)).thenReturn(query);
         when(query.getResultList()).thenReturn(resultList);
 
-        List<ProductReport> reports = reportService.getTopOrderedProducts(limit);
+        List<ProductReport> actual = reportService.getTopOrderedProducts(limit);
 
-        assertNotNull(reports);
-        assertEquals(2, reports.size());
-        assertEquals(1L, reports.get(0).getProduct().getProductId());
-        assertEquals(5L, reports.get(0).getQuantity());
-        verify(entityManager).createNativeQuery(anyString());
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertEquals(expected, actual);
     }
+
     @Test
-    @DisplayName("Getting top N most canceled products")
-    void getTopCanceledProducts_ShouldReturnList() {
-        int limit = 2;
-        Object[] row1 = new Object[]{3L, "Product 3", BigDecimal.valueOf(30), BigDecimal.valueOf(25), 3L, "Desc 3", "url3", 4L};
-        List<Object[]> resultList = new ArrayList<>();
-        resultList.add(row1);
+    @DisplayName("Get most canceled products")
+    void getTopCanceledProducts() {
+        Integer limit = 3;
+        List<Object[]> resultList = List.<Object[]>of(
+                new Object[]{
+                        1L,
+                        "All-Purpose Plant Fertilizer",
+                        BigDecimal.valueOf(8.99),
+                        BigDecimal.valueOf(11.99),
+                        1L,
+                        "Balanced NPK formula for all types of plants",
+                        "https://example.com/images/fertilizer_all_purpose.jpg",
+                        1L
+                });
+
+        List<ProductReport> expected = List.of(ProductReport.builder()
+                .product(productShortResponseDto1)
+                .quantity(1L)
+                .build());
 
         when(entityManager.createNativeQuery(anyString())).thenReturn(query);
         when(query.setParameter("status", OrderStatus.CANCELLED.name())).thenReturn(query);
         when(query.setParameter("limit", limit)).thenReturn(query);
         when(query.getResultList()).thenReturn(resultList);
 
-        List<ProductReport> reports = reportService.getTopCanceledProducts(limit);
+        List<ProductReport> actual = reportService.getTopCanceledProducts(limit);
 
-        assertNotNull(reports);
-        assertEquals(1, reports.size());
-        assertEquals(3L, reports.get(0).getProduct().getProductId());
-        assertEquals(4L, reports.get(0).getQuantity());
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("Getting products awaiting payment for more than N days")
-    void getProductsAwaitingPaymentForMoreDays_ShouldReturnList() {
-
+    @DisplayName("Get products awaiting payment")
+    void getProductsAwaitingPaymentForMoreDays() {
         int days = 7;
         int limit = 5;
 
-        LocalDateTime fixedTime = LocalDateTime.of(2023, 1, 1, 12, 0);
+        LocalDateTime fixedTime = LocalDateTime.of(2025, 8, 1, 12, 0);
         LocalDateTime timeBefore = fixedTime.minusDays(days);
 
-        List<Object[]> resultList = List.of(
-                new Object[]{1L, "Product 1", BigDecimal.valueOf(10), BigDecimal.valueOf(8), 1L, "Desc 1", "url1", 2L},
-                new Object[]{2L, "Product 2", BigDecimal.valueOf(20), BigDecimal.valueOf(15), 2L, "Desc 2", "url2", 1L}
-        );
+        List<Object[]> resultList = List.<Object[]>of(
+                new Object[]{
+                        1L,
+                        "All-Purpose Plant Fertilizer",
+                        BigDecimal.valueOf(8.99),
+                        BigDecimal.valueOf(11.99),
+                        1L,
+                        "Balanced NPK formula for all types of plants",
+                        "https://example.com/images/fertilizer_all_purpose.jpg",
+                        1L
+                });
 
+        List<ProductReport> expected = List.of(ProductReport.builder()
+                .product(productShortResponseDto1)
+                .quantity(1L)
+                .build());
 
         when(entityManager.createNativeQuery(anyString())).thenReturn(query);
         when(query.setParameter(eq("timeBefore"), any(LocalDateTime.class))).thenReturn(query);
         when(query.setParameter("limit", limit)).thenReturn(query);
         when(query.getResultList()).thenReturn(resultList);
 
-        List<ProductReport> reports = reportService.getProductsAwaitingPaymentForMoreDays(days, limit);
+        List<ProductReport> actual = reportService.getProductsAwaitingPaymentForMoreDays(days, limit);
 
-        assertNotNull(reports);
-        assertEquals(2, reports.size());
-        assertEquals(1L, reports.get(0).getProduct().getProductId());
-        assertEquals(2L, reports.get(0).getQuantity());
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertEquals(expected, actual);
     }
+
     @Test
     @DisplayName("Getting profit report grouped by months")
     void getProfitOverPeriod_GroupByMonth_ShouldReturnList() {
@@ -116,38 +149,30 @@ class ReportServiceImplTest extends AbstractTest {
         String groupBy = "month";
         LocalDateTime start = LocalDateTime.now().minusMonths(timeAmount);
 
-        List<Object[]> resultList = Arrays.asList(
-                new Object[]{BigDecimal.valueOf(1000), "2023-01", "2023-01 W1", "2023-01-01", "2023-01-01 12:00"},
-                new Object[]{BigDecimal.valueOf(1500), "2023-02", "2023-02 W1", "2023-02-01", "2023-02-01 12:00"}
+        List<Object[]> resultList = List.<Object[]>of(
+                new Object[]{
+                        BigDecimal.valueOf(11.50),
+                        "2025-05",
+                        "2023-05 W1",
+                        "2023-05-05",
+                        "2023-05-05 17:00"}
+        );
+
+        List<ProfitReport> expected = List.of(
+                ProfitReport.builder()
+                        .period("2025-05")
+                        .amountPerPeriod(BigDecimal.valueOf(11.50))
+                        .build()
         );
 
         when(entityManager.createNativeQuery(anyString())).thenReturn(query);
         when(query.setParameter(eq("timeBefore"), any(LocalDateTime.class))).thenReturn(query);
         when(query.getResultList()).thenReturn(resultList);
 
-        List<ProfitReport> reports = reportService.getProfitOverPeriod(timeUnit, timeAmount, groupBy);
+        List<ProfitReport> actual = reportService.getProfitOverPeriod(timeUnit, timeAmount, groupBy);
 
-        assertNotNull(reports);
-        assertEquals(2, reports.size());
-        assertEquals("2023-01", reports.get(0).getPeriod());
-        assertEquals(BigDecimal.valueOf(1000), reports.get(0).getAmountPerPeriod());
-    }
-
-    @Test
-    @DisplayName("Getting profit report with unsupported period")
-    void getProfitOverPeriod_UnsupportedTimeUnit_ShouldUseDefault() {
-
-        String timeUnit = "unsupported";
-        int timeAmount = 1;
-        String groupBy = "day";
-
-        when(entityManager.createNativeQuery(anyString())).thenReturn(query);
-        when(query.setParameter(anyString(), any())).thenReturn(query);
-        when(query.getResultList()).thenReturn(List.of());
-
-        List<ProfitReport> reports = reportService.getProfitOverPeriod(timeUnit, timeAmount, groupBy);
-
-        assertNotNull(reports);
-        assertTrue(reports.isEmpty());
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertEquals(expected, actual);
     }
 }

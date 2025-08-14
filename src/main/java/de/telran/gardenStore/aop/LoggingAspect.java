6f00ced720ffa827c2574dec12956df9326d9654
NatKond <natzubova@gmail.com>
2025-08-14 16:24:55 +0200

    GSP-93 Fixed mistake with toString method in UserCreateRequestDto

--- src/main/java/de/telran/gardenStore/aop/LoggingAspect.java
+++ src/main/java/de/telran/gardenStore/aop/LoggingAspect.java
@@ -1,9 +1,11 @@
 package de.telran.gardenStore.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -12,11 +14,10 @@ import java.util.Arrays;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("within(@org.springframework.web.bind.annotation.RestController *) && " +
-            "execution(* *..*.*(..)) && " +
+    @Pointcut( "within(de.telran.gardenStore.controller..*) && " +
             "@annotation(org.springframework.web.bind.annotation.PostMapping)")
     public void postMethods() {}
 
@@ -26,13 +27,12 @@ public class LoggingAspect {
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
 
-        logger.info(">>> POST request parameters {}.{}(): {}",
+        log.info(">>> POST request parameters {}.{}(): {}",
                 className, methodName, Arrays.toString(args));
-
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null) {
-            logger.info("User: {}, Roles: {}",
+            log.info("User: {}, Roles: {}",
                     authentication.getName(),
                     authentication.getAuthorities());
         }
@@ -40,7 +40,7 @@ public class LoggingAspect {
 
     @AfterReturning(pointcut = "postMethods()", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        logger.info("<<< Execution result {}: {}",
+        log.info("<<< Execution result {}: {}",
                 joinPoint.getSignature().toShortString(),
                 result);
     }
